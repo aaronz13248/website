cb4f67cd3f2820171706888d6609b6fb0d4e1660
aaronz13248 <aaronz13248@gmail.com>
2022-04-27 14:06:51 -0400

    init

--- eatCookie.js
+++ eatCookie.js
@@ -3,7 +3,7 @@ function eatCookie() {
 }
 
     let x = document.cookie;
-    if (x == "ateCookie=true") {
+    if (x === "ateCookie=true") {
         document.getElementById("title").classList.add("hidden");
         document.getElementById("title2").classList.remove("hidden");
     }
