a430d259a084c96861cbffdb099c505e451f6646
aaronz13248 <aaronz13248@gmail.com>
2022-04-29 12:49:59 -0400

    init

--- navMenu.js
+++ navMenu.js
@@ -1,14 +1,14 @@
 function openNav() {
-    document.getElementById("navMenu").style.height = "3000px";
-    document.getElementById("navMenu").style.width ="200px";
-    document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("main").style.marginRight = "200px";
+    document.getElementById("navMenu").style.height = "1500px";
+    document.getElementById("navMenu").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginRight = "150px";
     document.getElementById("openBtn").classList.add("hidden");
   }
 
   function closeNav() {
-    document.getElementById("navMenu").style.height = "0%";
-    document.getElementById("navMenu").style.width = "0%";
+    document.getElementById("navMenu").style.height = "0px";
+    document.getElementById("navMenu").style.width = "0px";
     document.getElementById("main").style.marginLeft = "100px";
     document.getElementById("main").style.marginRight = "300px";
     setTimeout (function() {
