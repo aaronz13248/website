43992a404c78da020dbfd245eef87d17a37efc3c
aaronz13248 <aaronz13248@gmail.com>
2022-05-02 00:44:05 -0400

    js comments

--- javascript/eatCookie.js
+++ javascript/eatCookie.js
@@ -4,16 +4,16 @@ Date: May 1 2022
 Description: Handles the use of a cookie to change text on the website.
 */
 
-function eatCookie() {
+function eatCookie() { // Sets a cookie
     document.cookie = "ateCookie=true; path=/";
 }
 
-function vomitCookie() {
+function vomitCookie() { // Deletes the cookie by marking it as five decades past expiry date
     document.cookie = "ateCookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
 }
 
     let x = document.cookie;
-    if (x === "ateCookie=true") {
+    if (x === "ateCookie=true") { // If the cookie is set, show the alt title and "funtext"
         document.getElementById("title2").classList.remove("hidden");
         document.getElementById("funtext").classList.remove("hidden");
     }
--- javascript/navMenu.js
+++ javascript/navMenu.js
@@ -4,7 +4,7 @@ Date: May 1 2022
 Description: Handles opening and closing the sidebar navigaton menu.
 */
 
-function openNav() {
+function openNav() { // Sets margins 125px to the right, expands the navbar into visibility, then hides open button
     document.getElementById("navMenu").style.height = "1500px";
     document.getElementById("navMenu").style.width = "200px";
     document.getElementById("main").style.marginLeft = "225px";
@@ -12,7 +12,7 @@ function openNav() {
     document.getElementById("openBtn").classList.add("hidden");
   }
 
-  function closeNav() {
+  function closeNav() { // Sets margins 125px to the left, shrinks the navbar into oblivion, then shows open button
     document.getElementById("navMenu").style.height = "0px";
     document.getElementById("navMenu").style.width = "0px";
     document.getElementById("main").style.marginLeft = "100px";
